af3176cbc53d7e7fa00fb3c1e5023490fbbb8b26
owajbhai <owajshaikhrcm@gmail.com>
2025-04-12 00:40:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,11 +69,11 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
-REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/PzxFq6R0/43400e676b5e.jpg https://i.ibb.co/PzxFq6R0/43400e676b5e.jpg')).split()
+FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/PzxFq6R0/43400e676b5e.jpg')
+REFER_PICS = (environ.get("REFER_PICS", "https://i.ibb.co/PzxFq6R0/43400e676b5e.jpg")).split() 
+PAYPICS = (environ.get('PAYPICS', 'https://i.ibb.co/PzxFq6R0/43400e676b5e.jpg')).split()
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://i.ibb.co/PzxFq6R0/43400e676b5e.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
